94cd261d65220970c77e68de6c3cc565af7b5437
Henok Shewakena <henyvibe@gmail.com>
2022-12-12 18:49:10 +0300

    pushpall

--- monty.h
+++ monty.h
@@ -65,4 +65,6 @@ void call_oper(stack_t **stack, char *oper, unsigned int line);
 void instruct_push(stack_t **stack, unsigned int line);
 void instruct_pall(stack_t **stack, unsigned int line);
 
+int check_isdigit(char *str);
+
 #endif
